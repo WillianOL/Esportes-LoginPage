75048966eed2d596765351ff5f376376ef1d01ab
Will Oliveira <112639055+WillianOL@users.noreply.github.com>
2023-02-07 17:34:34 -0300

    Atualizações

--- assets/erroLogin.js
+++ assets/erroLogin.js
@@ -1,2 +1,12 @@
 const formulario = document.getElementById('form');
-const inputs = document.querySelectorAll('.required')
+const dadosLogin = document.querySelectorAll('.loginInputs')
+const conteinerLogin = document.querySelectorAll('.loginDados')
+const regexEmail = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i;
+
+dadosLogin[0].addEventListener('input', () => {
+    if(dadosLogin[0].value === regexEmail){
+        conteinerLogin[0].classList.add('loginDadosERRO')
+    } else{
+        conteinerLogin[0].classList.remove('loginDadosERRO')
+    }
+});
