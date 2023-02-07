3a107c95092b654b963244f33a99395280f4c2dc
Will Oliveira <112639055+WillianOL@users.noreply.github.com>
2023-02-07 18:37:46 -0300

    Atualizações

--- assets/erroLogin.js
+++ assets/erroLogin.js
@@ -1,6 +1,14 @@
 const formulario = document.getElementById('form');
 const dadosLogin = document.querySelectorAll('.loginInputs')
-const conteinerLogin = document.querySelectorAll('.loginDados')
+const conteinerLogin = document.querySelectorAll('.loginDados') 
+
+dadosLogin[1].addEventListener('input', () => {
+    if(dadosLogin[1].value.length < 8){
+        conteinerLogin[1].classList.add('loginSenhaERRO')
+    } else{
+        conteinerLogin[1].classList.remove('loginSenhaERRO')
+    }
+});
 
 dadosLogin[0].addEventListener('input', () => {
     if(dadosLogin[0].value.length < 3){
@@ -10,10 +18,3 @@ dadosLogin[0].addEventListener('input', () => {
     }
 });
 
-dadosLogin[1].addEventListener('input', () => {
-    if(dadosLogin[1].value.length < 8){
-        conteinerLogin[1].classList.add('loginSenhaERRO')
-    } else{
-        conteinerLogin[1].classList.remove('loginSenhaERRO')
-    }
-});
